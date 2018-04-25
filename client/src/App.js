a7cb70f925c5d4755251c0c7cf75a4ef59c3659f
sunza5505 <sun.dodee55@gmail.com>
2018-04-25 16:17:42 +0700

    use react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 //import axios from 'axios';
 //import { createStore, applyMiddleware} from 'redux';
 //import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createStore, applyMiddleware } from 'redux';
+/*import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { fetchBear } from './actions';
+import { fetchBear } from './actions';*/
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 /*let bearReducer = function(state = [], action){
   if(action){
     if(action.type == 'FETCH_BEAR'){
@@ -20,7 +21,7 @@ let store = createStoreWithMiddleware(combineReducers({
     bear: bearReducer
   }))*/
 
-let store = createStoreWithMiddleware(reducers) 
+//let store = createStoreWithMiddleware(reducers) 
 
 /*let fetchBearActionCreator = function(){
   return (dispatch) => {
@@ -34,21 +35,22 @@ let store = createStoreWithMiddleware(reducers)
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {data: []};
+    /*this.state = {data: []};
   }
   componentDidMount(){
     /*axios.get('http://localhost:8000/api/bears')
     .then(result => {
       this.setState({data: result.data})
-    })*/
+    })
     store.subscribe(() => {
       this.setState({data: store.getState().bear})
     })
     //store.dispatch(fetchBearActionCreator());
-    store.dispatch(fetchBear());
+    store.dispatch(fetchBear());*/
   }
   render() {
-    let bears = this.state.data;
+    //let bears = this.state.data;
+    let bears = this.props.bear;
     return (
       //<div>Hello world</div>
       <div>
@@ -60,4 +62,8 @@ class App extends Component {
   }
 }
 
-export default App;
+//export default App;
+let mapStateToProps = (state) => (
+  {bear: state.bear}
+)
+export default connect(mapStateToProps)(App);
