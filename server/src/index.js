63bd10e34b8c3af6f338699e525e3f6d23eb0f7e
sunza5505 <sun.dodee55@gmail.com>
2018-04-26 23:17:02 +0700

    server support deleting

--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var bears = [
-    { id: 1, name: "Gerrard" },
-    { id: 2, name: "Salah" }
+    { id: '1', name: 'Gerrard' },
+    { id: '2', name: 'Salah' }
 ];
 
 router.route('/bears')
@@ -18,7 +18,12 @@ router.route('/bears')
         bear.name = req.body.name;
         bears.push(bear);
         res.json({ message: 'Bear created!' });
-    });
+    })
+    router.route('/bear/:id')
+    .delete(function(req, res){
+        bears = bear.filter(b => b.id !== req.param.id)
+        res.json({ message: 'Bear delete!'});
+    })
 
 app.use(cors());
 // all of our routes will be prefixed with /api
